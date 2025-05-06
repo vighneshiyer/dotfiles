de67cdae9f8ca333be5d0593605afbcf3cbb5cd1
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2025-05-06 16:36:25 -0700

    mason fix

---
+++ dot_config/nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    version = "1.11.0",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "black",
+        "pyright",
+        "ruff-lsp",
+        "rust-analyzer",
+        "lua-language-server",
+        "marksman",
+        "tinymist",
+      },
+    },
+  },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+}
